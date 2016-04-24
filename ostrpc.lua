9294c625d0a164187c69bbda44d8147e5c08ec06
Srivats P <pstavirs@users.noreply.github.com>
2016-04-24 20:09:30 +0530

    Implemented TCP reassembly (if reqd.) before dissecting

--- ostrpc.lua
+++ ostrpc.lua
@@ -89,6 +89,8 @@ do
     local p_ostrpc = Proto("ostrpc","Ostinato RPC");
 
     -- RPC Header (Sub)Proto and Field(s)
+    -- Header Format (8 bytes): MsgType(2), Method(2), Length(4)
+    -- Note: Length excluding header
     local p_hdr = Proto("ostrpc.header","RPC Header")
     local f_type = ProtoField.uint16("ostrpc.type","Type",base.DEC, types)
     local f_method = ProtoField.uint16("ostrpc.method","Method",base.DEC,
@@ -107,23 +109,52 @@ do
         end))
     end
 
-    function p_ostrpc.dissector(buf, pinfo, root)
+    function ostrpc_pdu_len(buf, pinfo, offset)
+        -- validate hdr
+        local msg_type = buf(0,2):uint()
+        local method = buf(2,2):uint()
+        if (types[msg_type] == nil or methods[method] == nil) then
+            info("invalid msgType or method")
+            return -1
+        end
+
+        return 8 + buf(4,4):uint()
+    end
+
+    function ostrpc_dissector(buf, pinfo, root)
+        info("tvb len = "..buf:len())
+
+        -- TODO: different cap_len and reported_len
+
         -- add OST-RPC tree
         local t = root:add(p_ostrpc, buf(0,buf:len()))
 
-        -- add RPC Header subtree to OST-RPC tree
-        local hdr = t:add(p_hdr, buf(0,8))
         local msg_type = buf(0,2):uint()
         local method = buf(2,2):uint()
         local length = buf(4,4):uint()
 
+        -- validate hdr
+        local msg_type = buf(0,2):uint()
+        local method = buf(2,2):uint()
+        if (types[msg_type] == nil or methods[method] == nil) then
+            info("[invalid msgType or method]")
+            if ProtoExpert then
+                local err = ProtoExpert("", "Malformed RPC Header",
+                                        expert.group.MALFORMED,
+                                        expert.severity.ERROR)
+                t:add(err, buf(0,8))
+            end
+            return 0
+        end
+
+        -- add RPC Header subtree to OST-RPC tree
+        local hdr = t:add(p_hdr, buf(0,8))
         hdr:add(f_type, buf(0,2), msg_type)
         hdr:add(f_method, buf(2,2), method)
         hdr:add(f_len, buf(4,4), length)
 
         hdr:append_text(": "..types[msg_type].." "..methods[method])
         t:append_text(", "..types2[msg_type].." "..methods[method])
-        info("1")
 
         -- find the Message corresponding to the RPC method and msg_type
         local pbmsg = ""
@@ -141,25 +172,17 @@ do
 
         -- local msg = rpc.VersionInfo():Parse(byt)
 
-        -- TODO: do we have entire message in this packet
-        -- or do we need reassembly?
-        -- see https://wiki.wireshark.org/Lua/Dissectors#TCP_reassembly
-
         -- build the 'program' to parse the suitable Message
         local prog = assert(loadstring("return rpc."..pbmsg.."():Parse(...)"))
-        info("2")
 
         -- extract the serialized data to parse
         local byt = tostring(buf(8, length):bytes()):fromhex()
-        info("2a")
 
         -- Actually parse the serialized binary data using the built 'program'
         local msg = prog(byt)
-        info("3")
 
         -- Serialize to human-readable text
         local txt= msg:Serialize("text")
-        info("4")
 
         -- add RPC Data subtree to OST-RPC tree
         local data = t:add(p_data, buf(8, length))
@@ -181,6 +204,57 @@ do
         -- TODO: instead of pbmsg use actual [elided] data
         pinfo.cols.protocol = "OST-RPC"
         pinfo.cols.info = types2[msg_type]..": "..methods[method].." ("..pbmsg..")"
+    end
+
+    function tcp_dissect_pdu(buf, pinfo, tree, pdu_hdr_len,
+                             get_pdu_len, pdu_dissector)
+        local offset = 0
+        local len = buf:len()
+        local pdu_len = 0
+
+        while (len > 0) do
+            info("#"..pinfo.number.." len: "..pinfo.len)
+            if (len < pdu_hdr_len) then
+                -- don't have full header, need more bytes
+                pinfo.desegment_offset = offset
+                pinfo.desegment_len = DESEGMENT_ONE_MORE_SEGMENT
+                info("incomplete pdu hdr ["..offset..", +1]")
+                return
+            end
+
+            pdu_len = get_pdu_len(buf, pinfo, offset)
+            if (pdu_len < pdu_hdr_len) then
+                info("invalid msgType or method")
+                if ProtoExpert then
+                    local err = ProtoExpert("", "Malformed RPC Header",
+                                            expert.group.MALFORMED,
+                                            expert.severity.ERROR)
+                    tree:add(err, buf(offset,8))
+                end
+                return 0
+            end
+            if (len < pdu_len) then
+                -- don't have full PDU, need more bytes
+                pinfo.desegment_offset = offset
+                pinfo.desegment_len = pdu_len - len
+                info("incomplete pdu ["..offset..", "..pdu_len-len.."]")
+                return;
+            end
+
+            info("calling pdu dissector ["..offset..", "..pdu_len.."]")
+            pdu_dissector(buf(offset, pdu_len):tvb(), pinfo, tree)
+
+            offset = offset + pdu_len
+            len = len - pdu_len
+        end
+
+    end
+
+    function p_ostrpc.dissector(buf, pinfo, root)
+        -- recent version of Wireshark have a tcp_dissect_pdus() but
+        -- for some reason it doesn't seem to accept pinfo as a param
+        return tcp_dissect_pdu(buf, pinfo, root,
+                               8, ostrpc_pdu_len, ostrpc_dissector)
     end
 
     local tcp_encap_table = DissectorTable.get("tcp.port")
